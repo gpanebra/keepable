fb03b00617d3ce77183694650872e5bb728f04d0
robertmedinasb <robertmedib1@gmail.co>
2020-06-18 18:41:30 -0500

    Adding location with styles into the header

--- geolocation.js
+++ geolocation.js
@@ -3,7 +3,6 @@ const position = navigator.geolocation.getCurrentPosition(success, error);
 
 async function request(location) {
     let url = `http://api.weatherapi.com/v1/forecast.json?key=48b388aa4e0b4192b27202932201806&q=${location}&days=1`
-    console.log(url)
     let response = await fetch(url);
     if (response.ok) {
         let json = await response.json();
@@ -12,16 +11,14 @@ async function request(location) {
         let country = json.location.country
         let temperature = json.forecast.forecastday[0].day.avgtemp_c
         let weather = json.forecast.forecastday[0].day.condition.text
-        let location = `${name}, ${province}, ${country}`
-        console.log(`Today's Forecast for ${location}: ${temperature}, ${weather}`)
-        showforecast(location, temperature, weather)
+        let message = `${name}, ${province}, ${country}`
+        showforecast(message, temperature, weather)
     } else {
         span_weather.textContent = "Sorry, we can't access to your location"
     }
 
 }
 function success(position) {
-    console.log(position)
     let latitude = position.coords.latitude
     let longitude = position.coords.longitude
     let location = `${latitude},${longitude}`
