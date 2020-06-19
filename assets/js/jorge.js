b2f0a139cbae02e8d7d825ced3065ee1caae289d
robertmedinasb <robertmedib1@gmail.co>
2020-06-19 04:01:41 -0500

    navigations links already finished

--- assets/js/jorge.js
+++ assets/js/jorge.js
@@ -1,33 +1,59 @@
 const createButton = document.querySelector(".keep-button")
 createButton.addEventListener("click", createCardHtml)
-const others = document.querySelector(".others")
+let others = document.querySelector(".others")
 const pallette_buttons = document.querySelectorAll(".pallette-button")
 pallette_buttons.forEach((element) => element.addEventListener("click", pallete))
+const trash_buttons = document.querySelectorAll(".pallette-button")
+trash_buttons.forEach((element) => element.addEventListener("click", deleteCard))
+let notes_nav = document.querySelector(".sidebar-notes")
+let trash_nav = document.querySelector(".sidebar-trash")
+notes_nav.addEventListener("click", navigation)
+trash_nav.addEventListener("click", navigation)
 
-let card_id = 0
+let wrapper = document.querySelector(".wrapper")
+messageWaiting();
+
+const cards_todo = []
+const cards_trashed = []
 function createCardHtml() {
-    card_id++
     let color = "color-1"
-    let content = document.querySelector("#body-note").value;
+    let content = document.querySelector("#body-note");
+    if (content.value.trim().length < 2) {
+        return alert("The note will be at least 2 words of length")
+    }
     const card = document.createElement("div");
+
     card.className = `card ${color}`;
-    card.id = `card-${card_id}`
+    card.id = `card-${cards_todo.length + 1}`
     card.innerHTML = `
-    <p class="card__content">${content}</p>
+    <p class="card__content">${content.value.trim()}</p>
     <div class="card__bottom">
-    <button class="pallette-button">
-        <img src="assets/images/pallette.svg" alt="">
+
+        <button class="pallette-button">
+            <img src="assets/images/pallette.svg" alt="">
+        </button>
+        <button class="trash-button">
+        <img src="assets/images/trash.svg" alt="">
     </button>
-      <i class="far fa-trash-alt card__icon"></i>
     </div>
     `
-    appendNoteToOthers(card)
+    card.querySelector(".pallette-button").addEventListener("click", pallete)
+    card.querySelector(".trash-button").addEventListener("click", deleteCard)
+
+    let card_created = {
+        card_id: card.id,
+        content: content.value.trim(),
+        color: color
+    }
+    cards_todo.push(card_created)
+
+    return appendNoteToOthers(card, content)
 }
 
-function appendNoteToOthers(element) {
+function appendNoteToOthers(element, content) {
     others.prepend(element)
-    let content = document.querySelector("#body-note");
     content.value = ""
+    messageWaiting();
 }
 function pallete() {
     let currentcard = this.parentNode.parentNode
@@ -49,4 +75,58 @@ function addPalleteHtml(currentcard) {
         pallet.append(color)
     }
     currentcard.append(pallet)
+}
+
+function messageWaiting() {
+
+    if (others.childElementCount == 0) {
+        let messageContainer = document.createElement("div");
+        messageContainer.className = "message-waiting"
+        messageContainer.innerHTML = `
+            <img src = "assets/images/{.svg" alt = "" >
+                <p> Notes you add appear here</p >
+                <img src = "assets/images/}.svg" alt = "" >
+        `
+        wrapper.append(messageContainer)
+        others.style.display = "none"
+    }
+    else {
+
+        others.style.display = "grid"
+        let message = wrapper.querySelector(".message-waiting")
+        if (message) {
+            message.remove()
+        }
+    }
+
+}
+function deleteCard() {
+    let currentcard = this.parentNode.parentNode
+    console.log(currentcard)
+    let content = currentcard.querySelector(".card__content").textContent
+    console.log(content)
+    let deleted_card = {
+        card_id: currentcard.id,
+        content: content,
+        color: currentcard.classList["1"]
+    }
+    let index = cards_todo.indexOf(currentcard.id)
+    cards_todo.splice(index, 1)
+    cards_trashed.push(deleted_card)
+    currentcard.remove()
+    return messageWaiting()
+}
+
+function navigation() {
+    let classes = Array.from(this.classList)
+    let lastClass = classes.pop()
+    if (lastClass == "sidebar-notes") {
+        document.querySelector(".sidebar-notes").classList.add("marked")
+        document.querySelector(".sidebar-trash").classList.remove("marked")
+        return shownotes() //TODO
+    } else {
+        document.querySelector(".sidebar-trash").classList.add("marked")
+        document.querySelector(".sidebar-notes").classList.remove("marked")
+        return showtrash() //TODO
+    }
 }
