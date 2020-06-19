367a15f71b95092c9dbd1746caad48362111647c
robertmedinasb <robertmedib1@gmail.co>
2020-06-19 01:30:57 -0500

    User can add a simple note

--- assets/js/jorge.js
+++ assets/js/jorge.js
@@ -1,23 +1,52 @@
-function createCardHtml(content,color) {
+const createButton = document.querySelector(".keep-button")
+createButton.addEventListener("click", createCardHtml)
+const others = document.querySelector(".others")
+const pallette_buttons = document.querySelectorAll(".pallette-button")
+pallette_buttons.forEach((element) => element.addEventListener("click", pallete))
+
+let card_id = 0
+function createCardHtml() {
+    card_id++
+    let color = "color-1"
+    let content = document.querySelector("#body-note").value;
     const card = document.createElement("div");
-    card.classList.add("card",color);
+    card.className = `card ${color}`;
+    card.id = `card-${card_id}`
     card.innerHTML = `
     <p class="card__content">${content}</p>
     <div class="card__bottom">
-      <i class="fas fa-palette card__icon"></i>
+    <button class="pallette-button">
+        <img src="assets/images/pallette.svg" alt="">
+    </button>
       <i class="far fa-trash-alt card__icon"></i>
     </div>
     `
-    return card
+    appendNoteToOthers(card)
 }
 
-function addPalleteHtml() {
+function appendNoteToOthers(element) {
+    others.prepend(element)
+    let content = document.querySelector("#body-note");
+    content.value = ""
+}
+function pallete() {
+    let currentcard = this.parentNode.parentNode
+    let pallete = currentcard.querySelector(".pallete-colors")
+    if (pallete) {
+        pallete.remove()
+    }
+    else {
+        addPalleteHtml(currentcard)
+    }
+
+}
+function addPalleteHtml(currentcard) {
     const pallet = document.createElement("div");
     pallet.classList.add("pallete-colors");
-    for(let i =1; i<=10;i++){
+    for (let i = 1; i <= 10; i++) {
         const color = document.createElement("div");
-        color.classList.add("color","color-"+i);
+        color.classList.add("color", "color-" + i);
         pallet.append(color)
     }
-    return pallet
+    currentcard.append(pallet)
 }
